3e373841a7bd3e80fdebeb534ea58b87628ad1ad
karrabi <30956008+karrabi@users.noreply.github.com>
2021-11-29 21:17:19 +0330

    get next period is done

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def main():
     a = dp.DataProvider(exchange='BINANCE', symbol='BTCUSDT')
     # print(a.getCryptoSymbols())
     # print(a.getCryptoSymbolCandles(symbol='BINANCE:BTCUSDT', _from=1637608665, _to=1638127065))
-    print(a.getPeriodicCryptoCandles(symbol='BINANCE:BTCUSDT', resolution='15', period=15))
+    print(a.getPeriodicCryptoCandles(symbol='BINANCE:BTCUSDT', resolution='5', period=15))
 
 
 if __name__ == '__main__':
